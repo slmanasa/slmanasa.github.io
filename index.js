e95253a4c70571f1b3bedaeaa5fe89f7e21ffd4b
manasa27 <manasachowdarychinna@gmail.com>
2018-11-22 08:47:32 +0530

    resum

---
+++ index.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    
+    <!-- html -->
+    $("#img1").hover(function(){
+        
+        $("#img1").attr("src","html5black.png");
+    })
+    
+    $("#img1").mouseout(function(){
+        
+        $("#img1").attr("src","html5.png");
+    })
+    
+    <!-- javascript -->
+    $("#img2").hover(function(){
+        
+        $("#img2").attr("src","jsblack.png");
+    })
+    
+    $("#img2").mouseout(function(){
+        
+        $("#img2").attr("src","js5.png");
+    })
+    
+    
+    <!-- jquery -->
+    $("#img3").hover(function(){
+        
+        $("#img3").attr("src","jqblack.png");
+    })
+    
+    $("#img3").mouseout(function(){
+        
+        $("#img3").attr("src","jq.png");
+    })
+    
+    <!-- bootstrap -->
+    $("#img4").hover(function(){
+        
+        $("#img4").attr("src","bsblack.png");
+    })
+    
+    $("#img4").mouseout(function(){
+        
+        $("#img4").attr("src","bs.png");
+    })
+    
+    <!-- angular -->
+    $("#img5").hover(function(){
+        
+        $("#img5").attr("src","angblack.png");
+    })
+    
+    $("#img5").mouseout(function(){
+        
+        $("#img5").attr("src","ang.png");
+    })
+    
+    <!-- responsive design -->
+    $("#img6").hover(function(){
+        
+        $("#img6").attr("src","reblack.png");
+    })
+    
+    $("#img6").mouseout(function(){
+        
+        $("#img6").attr("src","re.png");
+    })
+    
+    
+})
